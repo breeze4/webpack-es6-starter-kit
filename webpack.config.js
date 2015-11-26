3cb3a6f3062a715c551296c158dbcd8464de1f46
Parkle Lee <phparkle@gmail.com>
2015-11-26 23:03:10 +0800

    Reformat javascript
    Use double quotes and add trailing commas

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,28 @@
-var path = require('path');
-var webpack = require('webpack');
+var path = require("path");
+var webpack = require("webpack");
 
 module.exports = {
-    entry: './es6/main.js',
+    entry: "./es6/main.js",
     output: {
         path: __dirname,
-        filename: 'bundle.js'
+        filename: "bundle.js",
     },
     module: {
         loaders: [
             {
-                loader: 'babel-loader',
-                test: path.join(__dirname, 'es6'),
+                loader: "babel-loader",
+                test: path.join(__dirname, "es6"),
                 query: {
-                  presets: 'es2015',
+                    presets: "es2015",
                 },
-            }
-        ]
+            },
+        ],
     },
     plugins: [
-        // Avoid publishing files when compilation fails
-        new webpack.NoErrorsPlugin()
+        new webpack.NoErrorsPlugin(),
     ],
     stats: {
-        // Nice colored output
-        colors: true
+        colors: true,
     },
-    // Create Sourcemaps for the bundle
-    devtool: 'source-map',
+    devtool: "source-map",
 };
